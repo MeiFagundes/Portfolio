a30552ccbd2ddc2b1a7c7dc48fb1bcdaf852b4b4
Mei Fagundes <mei.fagundes@dtidigital.com.br>
2020-12-15 10:50:39 -0300

    Apply Open Sans font family

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mei Fagundes',
       theme: ThemeData(
+          fontFamily: 'OpenSans',
           primarySwatch: Colors.pink,
           visualDensity: VisualDensity.adaptivePlatformDensity,
-          buttonColor: Colors.pink[300],
-          iconTheme: IconThemeData(color: Colors.pink[400], size: 25)),
+          buttonColor: Colors.pinkAccent,
+          iconTheme: IconThemeData(color: Colors.pinkAccent, size: 25)),
       home: Home(),
     );
   }
