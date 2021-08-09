a39b2a1d69fc0a455acc57d53825c3309c5b500b
Mei Fagundes <MeiFagundes@outlook.com>
2021-08-09 02:14:02 -0300

    Implement responsive mobile layout

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -7,48 +7,89 @@ import 'package:meifagundesdotcom/views/home/project_list/project_list_view.dart
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final isMobileDevice = ApplicationUtil.isMobileDevice(context);
-
     return Scaffold(
-      body: Flex(
-        direction: isMobileDevice ? Axis.vertical : Axis.horizontal,
-        children: [
-          Spacer(),
-          Flexible(
-            flex: 5,
-            child: Container(
-              constraints: BoxConstraints(maxHeight: 650),
-              child: Column(
-                children: [
-                  Spacer(),
-                  Expanded(
-                    flex: 7,
-                    child: Profile(),
-                  ),
-                  Spacer(),
-                  TextButton(
-                    onPressed: () => UrlUtil.openURI(UrlUtil.urlMetaProject),
-                    child: Text(
-                      'Site desenvolvido em Flutter for Web',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey,
-                        letterSpacing: 0.7,
-                      ),
+        body: SafeArea(
+      child: ApplicationUtil.isMobileDevice(context)
+          ? buildMobileLayout(context)
+          : buildDesktopLayout(context),
+    ));
+  }
+
+  Widget buildMobileLayout(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 550,
+              child: Profile(),
+            ),
+            Divider(),
+            ProjectListView(),
+            buildSourceButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildDesktopLayout(BuildContext context) {
+    return Row(
+      children: [
+        Spacer(),
+        Flexible(
+          flex: 5,
+          child: SizedBox(
+            height: 750,
+            child: Column(
+              children: [
+                Spacer(),
+                Expanded(
+                  flex: 7,
+                  child: Profile(),
+                ),
+                Spacer(),
+                TextButton(
+                  onPressed: () => UrlUtil.openURI(UrlUtil.urlMetaProject),
+                  child: Text(
+                    'Site desenvolvido em Flutter for Web',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey,
+                      letterSpacing: 0.7,
                     ),
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all<EdgeInsets>(
-                          EdgeInsets.all(10)),
-                    ),
                   ),
-                ],
-              ),
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.all(10)),
+                  ),
+                ),
+              ],
             ),
           ),
-          Spacer(),
-          Expanded(flex: 10, child: ProjectListView()),
-        ],
+        ),
+        Spacer(),
+        Flexible(flex: 10, fit: FlexFit.loose, child: ProjectListView()),
+      ],
+    );
+  }
+
+  Widget buildSourceButton(BuildContext context) {
+    return TextButton(
+      onPressed: () => UrlUtil.openURI(UrlUtil.urlMetaProject),
+      child: Text(
+        'Site desenvolvido em Flutter for Web',
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: Colors.grey,
+          letterSpacing: 0.7,
+        ),
+      ),
+      style: ButtonStyle(
+        padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(10)),
       ),
     );
   }
