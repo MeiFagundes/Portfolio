fa14a6af6316a19461b955b0fb571c0c01273b8d
Mei Fagundes <MeiFagundes@outlook.com>
2021-10-01 07:31:07 -0300

    Get project list from Cloud Firestore

--- lib/models/project_model.dart
+++ lib/models/project_model.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ProjectModel {
   String title;
@@ -11,6 +13,23 @@ class ProjectModel {
       required this.description,
       required this.imagePath,
       required this.links});
+
+  static ProjectModel fromFirestoreDoc(
+      QueryDocumentSnapshot<Map<String, dynamic>> doc) {
+    var links = <ProjectLink>[];
+    doc.data()['links'].toList().forEach(
+          (link) => links.add(
+            ProjectLink.fromType(type: link['type'], uri: link['url']),
+          ),
+        );
+
+    return ProjectModel(
+      title: doc.data()['title'],
+      description: doc.data()['description'],
+      imagePath: doc.data()['imageUrl'],
+      links: links,
+    );
+  }
 }
 
 class ProjectLink {
@@ -20,4 +39,35 @@ class ProjectLink {
 
   ProjectLink(
       {required this.icon, required this.description, required this.uri});
+
+  ProjectLink.fromType({required String type, required String uri})
+      : this.icon = _getIconByType(type),
+        this.description = _getDescriptionByType(type),
+        this.uri = uri;
+
+  static String _getDescriptionByType(String type) {
+    switch (type) {
+      case 'launch':
+        return "Acessar";
+      case 'download':
+        return "Download";
+      case 'source':
+        return "Código Fonte";
+      default:
+        return "";
+    }
+  }
+
+  static IconData _getIconByType(String type) {
+    switch (type) {
+      case 'launch':
+        return Icons.download_rounded;
+      case 'download':
+        return Icons.download;
+      case 'source':
+        return Icons.code;
+      default:
+        return Icons.circle;
+    }
+  }
 }
--- lib/views/home/project_list/project_list_view.dart
+++ lib/views/home/project_list/project_list_view.dart
@@ -1,10 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:meifagundesdotcom/repositories/projects_repository.dart';
+import 'package:meifagundesdotcom/models/project_model.dart';
 import 'package:meifagundesdotcom/views/home/project_list/project_card.dart';
 import 'package:meifagundesdotcom/views/shared/text_styles.dart';
 import 'package:meifagundesdotcom/utils/application_util.dart';
 
-class ProjectListView extends StatelessWidget {
+class ProjectListView extends StatefulWidget {
+  @override
+  State<ProjectListView> createState() => _ProjectListViewState();
+}
+
+class _ProjectListViewState extends State<ProjectListView> {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  var firestoreFuture;
+
+  @override
+  void initState() {
+    firestoreFuture =
+        firestore.collection('portfolio projects').snapshots().first;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ApplicationUtil.isMobileDevice(context)
@@ -21,26 +37,45 @@ class ProjectListView extends StatelessWidget {
   }
 
   Widget buildBaseLayout(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 30),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 20),
-            child: Text(
-              'Projetos Pessoais',
-              style: TextStyles.title,
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              for (final project in ProjectsRepository.getProjects())
-                ProjectCard(project),
-            ],
-          ),
-        ],
-      ),
-    );
+    return FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        future: firestoreFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.data != null) {
+            final projetos = snapshot.data!.docs
+                .map(
+                  (doc) => ProjectModel.fromFirestoreDoc(doc),
+                )
+                .toList();
+
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      'Projetos Pessoais',
+                      style: TextStyles.title,
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      for (final project in projetos) ProjectCard(project),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          } else
+            return Center(
+              child: Container(
+                width: 50,
+                height: 50,
+                child: CircularProgressIndicator(),
+              ),
+            );
+        });
   }
 }
