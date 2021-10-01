b410d5f386b3b2061cdf4a1844f0b5be02e5cda7
Mei Fagundes <MeiFagundes@outlook.com>
2021-10-01 06:09:32 -0300

    Add Firebase Core

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:meifagundesdotcom/views/home/home_view.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp().catchError((e) {
+    print(e);
+  });
   runApp(MyApp());
 }
 
